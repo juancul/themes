bd4248df6720d24ad1a7050a24c862643624beec
muycul <48526137+muycul@users.noreply.github.com>
2021-05-06 18:30:58 -0500

    Update functions.php

--- neve-child/functions.php
+++ neve-child/functions.php
@@ -585,7 +585,7 @@ add_action( 'woocommerce_before_single_product', 'add_select_country_state', 15 
 //add_action( 'woocommerce_before_main_content', 'add_select_country_state', 15 );
 function add_select_country_state() {
     $prod_id = get_the_ID();
-    if( has_term( 'rayco', 'product_cat' ), $prod_id ) {
+    if( has_term( 'rayco', 'product_cat' , $prod_id) ) {
         
         echo '<div class="woocommerce-info">Selecciona tu departamento: '.do_shortcode('[vcwccr_country_selector]').'</div>';
     }
